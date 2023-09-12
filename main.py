f61de2df03584cfec18dbc020aac7f86459ccd9e
Itsnb03 <132834489+Itsnb03@users.noreply.github.com>
2023-09-12 19:09:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-#Your First python program
-print("HEllo World!")
-print("Hello Boy")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
-# print("HEllo World!")
+import os
 
-#Variables in Python
-
-a = 3
-print(a)
-b = 2
-print(b)
-a = 1.1
-print(a)
-c = True #false --> These are Boolean variables
-
-d = "Nitin"
-print(d)
-
-e = None
-print(e)
-
-
-#Typecasting
-
-f = "5"
-print (int(f) + 1 )
-g = 5
-print (g + 1)
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker 1.1. Created by Nitin")
+    while True:
+        x = input("Enter what you want me to speak: ")
+        if x == "q":
+            os.system("say 'bye bye friend' ")
+            break
+        command = f"say {x}"
+        os.system(command)
